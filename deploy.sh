9d354b1bf5e1c303c163eade1ec4055e4859b1e1
ductram <ductram@kms-technology.com>
2023-10-23 15:59:49 +0700

    save change

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,8 @@
-./mvnw install
-
+mvn clean install
 
 docker container stop service_b_container
 docker container rm service_b_container
 docker rmi service_b
 
 docker build -t service_b .
-docker run --network service_ab_network --name service_b_container -p 3031:8081 -d service_b
-
+docker run --name service_b_container -p 8081:8080 -d service_b
