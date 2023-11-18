56402e0a297fc0e84824ad64c3e74496eead549b
ductram <ductram@kms-technology.com>
2023-11-18 08:15:31 +0700

    [ci build] test_b_18

--- build_image.sh
+++ build_image.sh
@@ -4,5 +4,5 @@ docker container stop service_b_container
 docker container rm service_b_container
 docker rmi service_b
 
-docker build -t service_b .
+docker build -t service_b -f Dockerfile.dev .
 # docker run --name service_b_container -p 8081:8080 -d service_b
